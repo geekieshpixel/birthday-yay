fe0ebe0fbb74475c66f002ccaf8516fcb70b8081
geekieshpixel <162988559+geekieshpixel@users.noreply.github.com>
2025-10-01 22:47:47 +0530

    removed unnecessary comments and code blocks

--- scripts/effect.js
+++ scripts/effect.js
@@ -1,6 +1,6 @@
 $(window).load(function(){
-	$('.loading').fadeOut('fast');
-	$('.container').fadeIn('fast');
+    $('.loading').fadeOut('fast');
+    $('.container').fadeIn('fast');
 });
 $('document').ready(function(){
 		var vw;
@@ -107,12 +107,7 @@ $('document').ready(function(){
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
+        $('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
 		loopOne();
 		loopTwo();
 		loopThree();
